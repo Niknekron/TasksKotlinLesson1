526421d90cc34fa56eb7c893e4c43370e4cc3d31
Niknekron <niknekron@gmail.com>
2025-10-20 12:45:52 +0300

    TK-1-2 Modern variable and value

---
+++ src/main/kotlin/Lesson1/Lesson1-Task2.kt
@@ -0,0 +1,14 @@
+package org.example.Lesson1
+
+fun main() {
+    val numberOfOrders: Int = 75
+    val textOfGratitude: String = "Thank you for purchasing our products! "
+    var numberOfStoreEmployees: Int = 2000
+
+    println(numberOfOrders)
+    println(textOfGratitude)
+//    println(numberOfStoreEmployees)
+
+    numberOfStoreEmployees = 1999
+    println(numberOfStoreEmployees)
+}
