bdbefff1a5bdddadb70927b578ea63bde54d3c4b
Niknekron <niknekron@gmail.com>
2025-10-20 12:21:48 +0300

    TK-1-1 Create variables

---
+++ src/main/kotlin/Lesson_1/Lesson1_task1.kt
@@ -0,0 +1,9 @@
+package org.example.Lesson_1
+
+fun main() {
+    val numberOfOrders:Int = 75
+    val textOfGratitude:String = "Thank you for purchasing our products! "
+
+    println(numberOfOrders)
+    println(textOfGratitude)
+}
