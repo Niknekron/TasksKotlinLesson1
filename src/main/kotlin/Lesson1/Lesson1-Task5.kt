57fe0b9857db1d33a74d912193eba815dff4448b
Niknekron <niknekron@gmail.com>
2025-10-20 20:59:42 +0300

    TK-1-5 Displayed flight duration data

---
+++ src/main/kotlin/Lesson1/Lesson1-Task5.kt
@@ -0,0 +1,9 @@
+package org.example.Lesson1
+
+fun main() {
+    val totalTime: Int = 6480
+    val hours: String = "01"
+    val minutes: String = "48"
+    val seconds: String = "00"
+    println("$hours:$minutes:$seconds")
+}
