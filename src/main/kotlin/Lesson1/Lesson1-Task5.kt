57c40c42ee2259c17fd3c058de1cfc6edb11ce50
Niknekron <niknekron@gmail.com>
2025-10-21 20:32:27 +0300

    KS-1-5 Calculated the values and changed the output

--- src/main/kotlin/Lesson1/Lesson1-Task5.kt
+++ src/main/kotlin/Lesson1/Lesson1-Task5.kt
@@ -1,9 +1,9 @@
-package org.example.Lesson1
+    package org.example.Lesson1
 
 fun main() {
     val totalTime: Int = 6480
-    val hours: String = "01"
-    val minutes: String = "48"
-    val seconds: String = "00"
-    println("$hours:$minutes:$seconds")
+    val hours = totalTime / 3600
+    val minutes = (totalTime % 3600) / 60
+    val seconds = totalTime % 60
+    println(String.format("%02d:%02d:%02d", hours, minutes, seconds))
 }
