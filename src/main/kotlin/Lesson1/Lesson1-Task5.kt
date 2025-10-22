efb04c0a09d12ee4604933bdc1baf0b35b560b77
Niknekron <niknekron@gmail.com>
2025-10-23 01:49:54 +0300

    KS-1-5 Add Constant

--- src/main/kotlin/Lesson1/Lesson1-Task5.kt
+++ src/main/kotlin/Lesson1/Lesson1-Task5.kt
@@ -1,9 +1,11 @@
     package org.example.Lesson1
 
+    const val SECOND = 60
+
 fun main() {
     val totalTime: Int = 6480
-    val hours = totalTime / 3600
-    val minutes = (totalTime % 3600) / 60
-    val seconds = totalTime % 60
+    val hours = totalTime / (SECOND * SECOND)
+    val minutes = (totalTime % (SECOND * SECOND)) / SECOND
+    val seconds = totalTime % SECOND
     println(String.format("%02d:%02d:%02d", hours, minutes, seconds))
 }
