5cadb7505cde127d9f4219a99277d9e41b04fb8e
Niknekron <niknekron@gmail.com>
2025-10-22 12:38:25 +0300

    KS-1-1 Removed data output

--- src/main/kotlin/Lesson_1/Lesson1_task1.kt
+++ src/main/kotlin/Lesson_1/Lesson1_task1.kt
@@ -2,8 +2,5 @@ package org.example.Lesson_1
 
 fun main() {
     val numberOfOrders:Int = 75
-    val textOfGratitude:String = "Thank you for purchasing our products! "
-
-    println(numberOfOrders)
-    println(textOfGratitude)
+    val textOfGratitude:String = "Thank you for purchasing our products!"
 }
