6f582898b73098cf2df1e4fbf9ff6c3c5b70eb5b
Niknekron <niknekron@gmail.com>
2025-10-24 21:30:20 +0300

    KS-2-4 Corrected display of processes

--- src/main/kotlin/Lesson2/Lesson2-Task4.kt
+++ src/main/kotlin/Lesson2/Lesson2-Task4.kt
@@ -1,13 +1,13 @@
 package org.example.Lesson2
 
-const val POWER_OF_BUFF = 0.2
+const val POWER_OF_BUFF = 20
 
 fun main() {
     val crystalOre: Int = 7
     val ironOre: Int = 11
 
-    val buffCrystalOre = (crystalOre * POWER_OF_BUFF).toInt()
-    val buffIronore = (ironOre * POWER_OF_BUFF).toInt()
+    val buffCrystalOre = (crystalOre * POWER_OF_BUFF / 100).toInt()
+    val buffIronore = (ironOre * POWER_OF_BUFF / 100).toInt()
 
     println("Crystal Ore: $buffCrystalOre")
     println("Iron Ore: $buffIronore")
