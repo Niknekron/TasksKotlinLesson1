8f56c3303ff48a930ce8d9baa7a9a3e2df43cf06
Niknekron <niknekron@gmail.com>
2025-10-24 17:45:31 +0300

    KS-2-4 Calculated the amount of ore with the buff

---
+++ src/main/kotlin/Lesson2/Lesson2-Task4.kt
@@ -0,0 +1,14 @@
+package org.example.Lesson2
+
+const val POWER_OF_BUFF = 0.2
+
+fun main() {
+    val crystalOre: Int = 7
+    val ironOre: Int = 11
+
+    val buffCrystalOre = (crystalOre * POWER_OF_BUFF).toInt()
+    val buffIronore = (ironOre * POWER_OF_BUFF).toInt()
+
+    println("Crystal Ore: $buffCrystalOre")
+    println("Iron Ore: $buffIronore")
+}
