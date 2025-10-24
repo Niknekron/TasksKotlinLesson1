b905b465b8c77deee0b4f3b03138206e47e5de8c
Niknekron <niknekron@gmail.com>
2025-10-24 18:29:57 +0300

    KS-2-3 Created constants and changed the output

--- src/main/kotlin/Lesson2/Lesson2-Task3.kt
+++ src/main/kotlin/Lesson2/Lesson2-Task3.kt
@@ -1,19 +1,22 @@
 package org.example.Lesson2
 
+const val MINUTES_IN_HOUR = 60
+const val HOURS_IN_A_DAY = 24
+
 fun main() {
     val startInHours: Int = 9
     val startInMinutes: Int = 39
     val travelMinutes: Int = 457
 
-    val travelToHours = travelMinutes / 60
-    val remainingMinutes = travelMinutes % 60
+    val travelToHours = travelMinutes / MINUTES_IN_HOUR
+    val remainingMinutes = travelMinutes % MINUTES_IN_HOUR
 
     val totalMinutes = startInMinutes + remainingMinutes
 
-    val numberOfFullHours = totalMinutes / 60
-    val extrasMinute = totalMinutes % 60
+    val numberOfFullHours = totalMinutes / MINUTES_IN_HOUR
+    val extrasMinute = totalMinutes % MINUTES_IN_HOUR
 
-    val totalTimeToTravel = (startInHours + travelToHours + numberOfFullHours) % 24
+    val totalTimeToTravel = (startInHours + travelToHours + numberOfFullHours) % HOURS_IN_A_DAY
 
-    println(String.format("%02d:%02d", totalTimeToTravel, extrasMinute))
+    println("%02d:%02d".format(totalTimeToTravel, extrasMinute))
 }
