11441590325930f4be8bd341f94681999224f64d
Niknekron <niknekron@gmail.com>
2025-10-24 21:42:12 +0300

    KS-2-5 Rewrote part of the code

--- src/main/kotlin/Lesson2/Lesson2-Task5.kt
+++ src/main/kotlin/Lesson2/Lesson2-Task5.kt
@@ -4,17 +4,12 @@ import kotlin.math.abs
 import kotlin.math.pow
 
 fun main() {
-    val P: Double = 70000.0
-    val r: Double = 0.167
-    val n: Double = 1.0
-    val t: Double = 20.0
+    val initialDeposit: Double = 70000.0
+    val annualInterestRate: Double = 16.7
+    val years: Int = 20
 
-    val A = (P * (1 + r / n).pow(n*t))
+    val interestRate = annualInterestRate / 100
+    val finalEmount = (initialDeposit * (1 + interestRate).pow(years))
 
-    println("Total: %.3f".format(A))
-
-    val expectedResult: Double = 1536438.428
-    val comparisonOfResults: Boolean = abs(expectedResult - A) < 0.001
-
-    println(comparisonOfResults)
+    println("Total: %.3f".format(finalEmount))
 }
