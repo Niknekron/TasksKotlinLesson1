d92b1aa145ddce81fb476ad388b2f124345c00fe
Niknekron <niknekron@gmail.com>
2025-10-25 17:09:44 +0300

    KS-2-5 Moved the value into a constant and corrected the errors.

--- src/main/kotlin/Lesson2/Lesson2-Task5.kt
+++ src/main/kotlin/Lesson2/Lesson2-Task5.kt
@@ -1,15 +1,14 @@
 package org.example.Lesson2
-
-import kotlin.math.abs
 import kotlin.math.pow
+
+const val percentValue = 100
 
 fun main() {
     val initialDeposit: Double = 70000.0
     val annualInterestRate: Double = 16.7
     val years: Int = 20
 
-    val interestRate = annualInterestRate / 100
-    val finalEmount = (initialDeposit * (1 + interestRate).pow(years))
-
-    println("Total: %.3f".format(finalEmount))
+    val interestRate = annualInterestRate / percentValue
+    val finalAmount = initialDeposit * (1 + interestRate).pow(years)
+    println("Total: %.3f".format(finalAmount))
 }
