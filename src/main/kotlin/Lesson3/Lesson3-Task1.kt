bfae8cea331173c41eec178ceb4bbf42916bf1ce
Niknekron <niknekron@gmail.com>
2025-10-26 13:18:04 +0300

    KS-3-1  Reused a variable.

--- src/main/kotlin/Lesson3/Lesson3-Task1.kt
+++ src/main/kotlin/Lesson3/Lesson3-Task1.kt
@@ -3,9 +3,10 @@ package org.example.Lesson3
 fun main() {
     val greetings: String = "Hello our dear client"
     val name: String = "Aleks"
-    val morningGreetings: String = "$name, $greetings! It's time before lunch now"
-    val eveningGreeting: String = "$name, $greetings! It's now afternoon"
+    var finalGreetings: String = "$name, $greetings! It's time before lunch now"
 
-    println(morningGreetings)
-    println(eveningGreeting)
+    println(finalGreetings)
+
+    finalGreetings = "$name, $greetings! It's now afternoon"
+    println(finalGreetings)
 }
