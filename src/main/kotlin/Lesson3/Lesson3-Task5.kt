e2eac0440271495b44a36fcf105f32de6fd0c483
Niknekron <niknekron@gmail.com>
2025-10-26 16:58:04 +0300

    KS-3-5 Did the reverse process relatively KS-3-4

---
+++ src/main/kotlin/Lesson3/Lesson3-Task5.kt
@@ -0,0 +1,13 @@
+package org.example.Lesson3
+
+fun main() {
+    val availavleInformation: String = "[D2-D4;0]"
+    val startPoint: String = "D2"
+    val finalPoint: String = "D4"
+    val numberOfStep: Int = 0
+
+    println(availavleInformation)
+    println(startPoint)
+    println(finalPoint)
+    println(numberOfStep)
+}
