6cd4df5c2d8b2470deaef2783177a1c0812c8fe2
Niknekron <niknekron@gmail.com>
2025-10-26 15:04:20 +0300

    KS-3-3 I wrote an implementation with one call.

---
+++ src/main/kotlin/Lesson3/Lesson3-Task3.kt
@@ -0,0 +1,17 @@
+package org.example.Lesson3
+
+fun main() {
+    val generalNumber: Int = 6
+
+    println(
+        "$generalNumber x 1 = ${generalNumber * 1}\n"+
+        "$generalNumber x 2 = ${generalNumber * 2}\n"+
+        "$generalNumber x 3 = ${generalNumber * 3}\n"+
+        "$generalNumber x 4 = ${generalNumber * 4}\n"+
+        "$generalNumber x 5 = ${generalNumber * 5}\n"+
+        "$generalNumber x 6 = ${generalNumber * 6}\n"+
+        "$generalNumber x 7 = ${generalNumber * 7}\n"+
+        "$generalNumber x 8 = ${generalNumber * 8}\n"+
+        "$generalNumber x 9 = ${generalNumber * 9}"
+    )
+}
