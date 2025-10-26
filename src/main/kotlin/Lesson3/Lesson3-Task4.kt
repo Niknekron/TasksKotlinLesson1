6335bf0b08c16c9f5a13ab54111e8584bf85eea7
Niknekron <niknekron@gmail.com>
2025-10-26 16:35:30 +0300

    KS-3-4 Added a description, bringing it to a simple template for the server.

---
+++ src/main/kotlin/Lesson3/Lesson3-Task4.kt
@@ -0,0 +1,17 @@
+package org.example.Lesson3
+
+fun main() {
+    var from: String = "E2"
+    var to: String = "E4"
+    var move: Int = 1
+    var actuallyInfrormation = "[$from-$to;$move]."
+
+    println(actuallyInfrormation)
+
+    from = "D2"
+    to = "D3"
+    move = 2
+    actuallyInfrormation = "[$from-$to;$move]."
+
+    println(actuallyInfrormation)
+}
