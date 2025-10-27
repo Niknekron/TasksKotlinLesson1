0d9039fcc81c69c5d124c3fb32dec35aa7349027
Niknekron <niknekron@gmail.com>
2025-10-27 14:33:46 +0300

    KS-3-5 Corrected the implementation

--- src/main/kotlin/Lesson3/Lesson3-Task5.kt
+++ src/main/kotlin/Lesson3/Lesson3-Task5.kt
@@ -2,14 +2,12 @@ package org.example.Lesson3
 
 fun main() {
     val availavleInformation: String = "D2-D4;0"
-    val parts = availavleInformation.split(";")
+    val parts = availavleInformation.split('-',';')
 
-    val fromTO = parts[0].split('-')
-    val from = fromTO[0]
-    val to = fromTO[1]
-    val steps = parts[1].toInt()
+    val from = parts[0]
+    val to = parts[1]
+    val steps = parts[2]
 
-    println(availavleInformation)
     println(from)
     println(to)
     println(steps)
