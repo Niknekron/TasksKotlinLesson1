21e0a29971e5a8020689b1afa6e76e6750220c29
Niknekron <niknekron@gmail.com>
2025-10-28 10:14:23 +0300

    KS-4-1 Fixxed errors.

--- src/main/kotlin/Lesson4/Lesson4-Task1.kt
+++ src/main/kotlin/Lesson4/Lesson4-Task1.kt
@@ -1,12 +1,14 @@
 package org.example.Lesson4
-const val SUM_TABLES: Int = 13
+
+const val TOTAL_TABLES: Int = 13
+
 fun main() {
     val reservationToday: Int = 13
     val reservationTomorrow: Int = 9
 
-    val isAvailableToday: Boolean = reservationToday < SUM_TABLES
-    val isAvailableTomorrow: Boolean = reservationTomorrow < SUM_TABLES
+    val isAvailableToday: Boolean = reservationToday < TOTAL_TABLES
+    val isAvailableTomorrow: Boolean = reservationTomorrow < TOTAL_TABLES
 
     println("[Table availability for today: $isAvailableToday]\n" +
-    "[Table availability for today: $isAvailableTomorrow]")
+    "[Table availability for tomorrow: $isAvailableTomorrow]")
 }
