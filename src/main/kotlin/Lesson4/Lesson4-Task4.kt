969e99f1c4637e770b63e00ec204666333523be4
Niknekron <niknekron@gmail.com>
2025-11-01 17:17:32 +0300

    KS-4-4  Created a program for selecting exercises by day.

---
+++ src/main/kotlin/Lesson4/Lesson4-Task4.kt
@@ -0,0 +1,16 @@
+package org.example.Lesson4
+
+fun main() {
+    val day = 5
+    val isArmsDay = day % 2 != 0
+    val isAbdominalDay = day % 2 != 0
+    val isLegsDay = day % 2 == 0
+    val isBackDay = day % 2 == 0
+
+    println(
+        "Hand exercises: $isArmsDay\n" +
+        "Leg exercises: $isLegsDay\n" +
+        "Back exercises: $isBackDay\n" +
+        "Abdominal exercises: $isAbdominalDay"
+    )
+}
